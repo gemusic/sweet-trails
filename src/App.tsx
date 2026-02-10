e188dfe8f7b5c9e64d675d4573fa74063fa56d23
gemusic <gegemusicking@gmail.com>
2026-02-10 02:01:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Index from "./pages/Index";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Events from "./pages/Events";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -26,7 +27,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/menu" element={<Menu />} />
             <Route path="/packages" element={<Menu />} />
-            <Route path="/events" element={<Contact />} />
+            <Route path="/events" element={<Events />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
